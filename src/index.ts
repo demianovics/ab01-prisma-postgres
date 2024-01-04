44fa9c1d6538aec6daabd1fc81db9638ac0f1a1c
Demian Grandt <demianovics@Air-von-Demian.fritz.box>
2024-01-04 15:01:22 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hallo Welt!');
+});
+
+app.listen(port, () => {
+  console.log(`Server läuft auf http://localhost:${port}`);
+});
