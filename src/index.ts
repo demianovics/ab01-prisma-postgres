b898c40d95e324b9cfc6f73af7882742b5cfc7a9
demianovics <demian.grandt@googlemail.com>
2024-01-04 16:20:30 +0100

    prisma user sample

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {
+    // const newUser = await prisma.user.create({
+    //     data: {
+    //         name: 'Alice',
+    //         email: 'alice2@example.com',
+    //     },
+    // });
+    // console.log('Neuer Benutzer:', newUser);
+
+    const users = await prisma.user.findMany();
+    console.log(users);
+}
+
+
+
 import express from 'express';
 
 const app = express();
@@ -5,6 +23,11 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hallo Welt!');
+});
+
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.send(users);
 });
 
 app.listen(port, () => {
