53ef0c496fe22622b90d583a67b4cb07d0356204
demianovics <demian.grandt@gmail.com>
2024-01-05 14:15:34 +0100

    npm run dev - uses nodemon npm run debug - uses nodemon and starts it with --inspect, so that VSCode can use .vscode/launch.json to launch and attach debugger

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,57 @@
 "use strict";
-console.log("ha");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const faker_1 = require("@faker-js/faker");
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient({
+    log: ['query', 'info', 'warn', 'error']
+});
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const newUser = yield prisma.user.create({
+            data: {
+                name: faker_1.faker.person.firstName(),
+                email: faker_1.faker.internet.email(),
+            },
+        });
+        console.log('Neuer Benutzer:', newUser);
+    });
+}
+main()
+    .catch((e) => {
+    throw e;
+})
+    .finally(() => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("disconnect");
+    yield prisma.$disconnect();
+}));
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Hallo Welt!');
+});
+app.get('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const users = yield prisma.user.findMany();
+        res.json(users);
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}));
+app.listen(port, () => {
+    console.log(`Server läuft auf http://localhost:${port}`);
+});
+//# sourceMappingURL=index.js.map
